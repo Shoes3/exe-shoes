4e8e24969057bf3182632253bd20d17147312143
Cecil <ccoupe@cableone.net>
2017-05-09 22:00:41 -0600

    for #4, GUI is OK enough (still some weirdness on page 5)
    * Good enough for our limited audience - dimishing returns
    * remove a lot of pixel counting. Could do more.
    * progress widget/windown would be cool - but kind of overkill.
    * need real world testing from shoes/windows developers.
    * testing is a real PITA.

--- build-exe.rb
+++ build-exe.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 
 Shoes.app(title: "Package app into exe", width: 600, height: 550, resizable: true) do
 	PWD = Dir.pwd
-	@offside, @edit_box_height = 15, 29 ### box dimmensions
+	@offside, @edit_box_height = 15, 20 ### box dimmensions
 	#@options = ["app_name", "app_version", "app_startmenu", "publisher", "website",
   #   "app_loc", "app_start", "app_ico", "include_gems", "installer_sidebar_bmp",
   #   "installer_header_bmp", "app_installer_ico", "hkey_org", "license" ]
@@ -74,33 +74,32 @@ Shoes.app(title: "Package app into exe", width: 600, height: 550, resizable: tru
 		@page = 1
 		subtitle "Application properties", align: "center"
 #		stack left: 128, top: 70, width: 0.5, height: 400 do
-		stack width: 0.8, height: 400 do
+#		stack width: 0.8, height: 400 do
+    stack height: 350 do
 			background darkgray
 			border black, strokewidth: 2
-			para "App name", margin_left: 32, margin_top: 20
-			@app_name = edit_box @values['app_name'],  height: @edit_box_height, margin_left: 32,
+			para "App name", margin_left: 32, margin_top: 10
+			@app_name = edit_line @values['app_name'], margin_left: 32,
           tooltip: "This and the SubName string will be used to name the installer File " do
         @values['app_name'] = @app_name.text
       end
-			(para "Application Subname").style(margin_top: @offside, margin_left: 32)
-			@app_version = edit_box @values['app_version'],  height: @edit_box_height, margin_left: 32,
+			para "Application Subname", margin_top: 10, margin_left: 32
+			@app_version = edit_line @values['app_version'], margin_left: 32,
           tooltip: "This is a string that is appended to App Name to name the installer file a hypthen will will be inserted" do
         @values['app_version'] = @app_version.text
       end
       
-#			(para "Start Menu folder name").style(margin_top: @offside, margin_left: 32)
-#			@app_startmenu = edit_box "Can be the same as App name", height: @edit_box_height, margin_left: 32
-			(para "Publisher name").style(margin_top: @offside, margin_left: 32)
+			para "Publisher name", margin_top: 10, margin_left: 32
       if !@values['publisher']
         @values['publisher'] = "My name here"
       end
-			@publisher =  edit_box @values['publisher'], height: @edit_box_height, margin_left: 32,
+			@publisher =  edit_line @values['publisher'], margin_left: 32,
           tooltip: "Your compony or org name" do
           @values['publisher'] = @publisher.text
       end
       
-			(para "Website").style(margin_top: @offside, margin_left: 32)
-			@website = edit_box @values['website'], height: @edit_box_height, margin_left: 32,
+			para "Website", margin_top: 10, margin_left: 32
+			@website = edit_line @values['website'], margin_left: 32,
           width: 0.8, tooltip: "url to your website" do
         @values['website'] = @website.text
       end
@@ -110,21 +109,13 @@ Shoes.app(title: "Package app into exe", width: 600, height: 550, resizable: tru
 	def page2
 		@page = 2
 		subtitle "Application source", align: "center"
-		stack left: 20, top: 70, width: 0.9, height: 280 do
+		stack  height: 280, width: 1.0 do
 			background darkgray
 			border black, strokewidth: 2
-			(para "Application folder").style(margin_top: @offside, margin_left: 32)
-			flow do
-				@app_loc = edit_box @values['app_loc'], width: 300, height: @edit_box_height, margin_left: 32, 
-          tooltip: "points to the top level folder of your Shoes app", state: "disabled"  
-				#button "Select folder" do
-				#	@app_loc.text = fix_string(ask_open_folder)
-        #  @values['app_loc'] = @app_loc.text
-				#end
-			end
-			(para "Starting script name").style(margin_top: @offside, margin_left: 32)
+			para "Starting script name", margin_top: 10, margin_left: 32
       flow do
-			  @app_start = edit_box @values['app_start'], width: 300, height: @edit_box_height, margin_left: 32,
+			  @app_start = edit_line @values['app_start'], width: 300, margin_left: 32,
+            margin_right: 4,
             tooltip: "The first script to run for your Shoes app"
         button "Select .rb file" do
           longfn = ask_open_file
@@ -135,10 +126,14 @@ Shoes.app(title: "Package app into exe", width: 600, height: 550, resizable: tru
           @values['app_loc'] = appdir
         end
       end
-			(para "Exe icon (.ico)").style(margin_top: @offside, margin_left: 32)
-			#@app_icon = get_file @app_icon, 32
+			#para "Application folder", margin_top: 10, margin_left: 32
+			para "Application folder", margin_left: 32
+      @app_loc = edit_line @values['app_loc'], width: 300, margin_left: 32, 
+          tooltip: "points to the top level folder of your Shoes app", state: "disabled"  
+			para "Exe icon (.ico)", margin_left: 32
 			flow do
-			  @app_icon = edit_box @values['app_ico'], width: 300, height: @edit_box_height, margin_left: 32,
+			  @app_icon = edit_line @values['app_ico'], width: 300, margin_left: 32,
+            margin_right: 4,
             tooltip: "The Window app icon for your Shoes app"
         button "Select .ico" do
           @app_icon.text = fix_string(ask_open_file)
@@ -197,54 +192,54 @@ Shoes.app(title: "Package app into exe", width: 600, height: 550, resizable: tru
 	def page4 
 		@page = 4
 		subtitle "NSIS Unique Settings", align: "center"
-		stack left: 40, top: 70, width: 0.81, height: 400 do
+		stack  height: 380 do
 			background darkgray
 			border black, strokewidth: 2
-			(para "Installer sidebar (164 x 309) .bmp").style(margin_top: @offside, margin_left: 20)
-      #@setup_side = get_file @setup_side, 20
+			para "Installer sidebar (164 x 309) .bmp", margin_left: 32
       flow do
-			  @setup_side = edit_box @values['installer_sidebar_bmp'], width: 300, height: @edit_box_height,
-          margin_left: 20
+			  @setup_side = edit_line @values['installer_sidebar_bmp'], width: 300,
+          margin_left: 32, margin_right:4,
+          tooltip: "The sidebar image (old bmp) for the installer"
 			  button "Select file" do
           @setup_side.text = ask_open_file
           @values['installer_sidebar_bmp'] = @setup_side.text
 			  end
 		  end
-			(para "Installer header pic (150 x 57) .bmp").style(margin_top: @offside, margin_left: 20)
-			#@setup_head = get_file @setup_head, 20
+			para "Installer header pic (150 x 57) .bmp", margin_left: 32
       flow do
-			  @setup_head = edit_box @values['installer_header_bmp'], width: 300, height: @edit_box_height,
-          margin_left: 20
+			  @setup_head = edit_line @values['installer_header_bmp'], width: 300, 
+          margin_left: 32, margin_right: 4,
+          tooltip: "The header image for the installer"
 			  button "Select file" do
           @setup_head.text = ask_open_file
           @values['installer_header_bmp'] = @setup_head.text
 			  end
 		  end
-      (para "Installer's icon (.ico)").style(margin_top: @offside, margin_left: 20)
-			#@setup_icon = get_file @setup_icon , 20
+      para "Installer's icon (.ico)", margin_left: 32
       flow do
-			  @setup_icon = edit_box @values['app_installer_ico'], width: 300, height: @edit_box_height,
-          margin_left: 20
+			  @setup_icon = edit_line @values['app_installer_ico'], width: 300, 
+          margin_left: 32,  margin_right: 4,
+          tooltip: "The icon for the #{@values['app_name']}-#{@values['app_version']}.exe file - NOT the icon for app desktop"
+          
 			  button "Select file" do
           @setup_icon.text = ask_open_file
           @values['app_installer_ico'] = @setup_icon.text
 			  end
 		  end
-      (para "hkey_org").style(margin_top: @offside, margin_left: 20)
-			#@setup_key = edit_box "mvmanila.com", width: 200, height: @edit_box_height, margin_left: 20
+      para "hkey_org", margin_left: 32
       if ! @values['hhey_org'] 
-        @values['kkey_org'] = "mvmanila.com"
+        @values['hkey_org'] = "mvmanila.com"
       end
-			@setup_key = edit_box @values['hkey_org'], width: 200, height: @edit_box_height, margin_left: 20,
-          tooltip: "don't change this unless you love pain. Just saying. Don't " do
-        edit_box @values['hkey_org'] = @setup_key.text
+			@setup_key = edit_line @values['hkey_org'], width: 300, margin_left: 32,
+          tooltip: "don't change this unless you know what it means" do
+        edit_line @values['hkey_org'] = @setup_key.text
       end
-			(para "Append to License").style(margin_top: @offside, margin_left: 20)
-			#@setup_lic = get_file @setup_head, 20
-			#@setup_lic.text = "#{PWD}/ytm/Ytm.license"
+			para "Pre-pend to License", margin_left: 32
       flow do
-        @setup_lic = edit_box @values['license'], width: 300, height: @edit_box_height,margin_left: 20,
-          tooltip: "prepend the contents to the Shoes LICSENSE.txt file"
+        @setup_lic = edit_line @values['license'], width: 300,margin_left: 32,
+          margin_right: 4,
+          tooltip: "prepend the contents to the Shoes LICSENSE.txt file\n \
+Will be shown to the User at install time, so make it pretty"
         button "Select file" do
           @setup_lic.text = ask_open_file
           @values['license'] = @setup_lic.text
@@ -263,31 +258,38 @@ Shoes.app(title: "Package app into exe", width: 600, height: 550, resizable: tru
     @values['include_gems'].each do |g|
       path = @gs_filep[g]
       if path
-        $stderr.puts "looking for #{g} found #{path}"
+        #$stderr.puts "looking for #{g} found #{path}"
         nv['include_gems'] << File.basename(path, '.gemspec')
       else
-        $stderr.puts "Gem not available #{g}"
+        alert "Gem not available #{g} check your .yaml"
+        #$stderr.puts "Gem not available #{g}"
       end
     end
-		flow do
-			para nv.to_yaml
-		end
-		#@next.remove
-		button "Save confg" do
-			File.open(ask_save_file, "w") { |f| f.write(nv.to_yaml) } 
-		end
-		@go_btn = button "Create .exe", left: 400 	do
-      require_relative 'merge-exe'
-      @go_btn.state = "disabled"
-      Shoes.terminal
-      # clever people could create a new shoes window/app (different OS thread)
-      # with a progess bar in it, maybe a status message area and pass something
-      # into an optional arg to Package::merge_exe that it can call to update the
-      # the progress widget and the status text. Like the Gem install does. 
-      PackShoes::merge_exe nv
-      @go_btn.state = nil # nil enables button - legacy
-      # create a quit button to let user know that it's time to end this.
-		end
+    stack do
+      flow do 
+  		  button "Save confg" do
+	    		File.open(ask_save_file, "w") { |f| f.write(nv.to_yaml) } 
+    		end
+  		  @go_btn = button "Create .exe", left: 400 	do
+          require_relative 'merge-exe'
+          @go_btn.state = "disabled"
+          Shoes.terminal
+          # clever people could create a new shoes window/app (different OS thread)
+          # with a progess bar in it, maybe a status message area and pass something
+          # into an optional arg to Package::merge_exe that it can call to update the
+          # the progress widget and the status text. Like the Gem install does. 
+          PackShoes::merge_exe nv
+          @go_btn.state = nil # nil enables button - legacy
+          # create a quit button to let user know that it's time to end this.
+	  	  end
+      end
+    end
+    # display yaml
+    stack do
+		  flow do # depends on yaml having nl's embedded
+			  para nv.to_yaml, stroke: white
+		  end
+    end
 	end
 	
   # create the outer gui - button bar, panel for page(n) content
@@ -311,7 +313,8 @@ Shoes.app(title: "Package app into exe", width: 600, height: 550, resizable: tru
         @frame.clear { @pages[np-1].call }
       end
     end
-    @frame = flow margin: 0.05, width: 0.9 do
+    #@frame = flow margin: 0.05 do #, width: 0.9 do
+    @frame = stack margin: 16 do
       @pages[0].call
     end
 #    @next = button "Next", left: 500, top: 500 do
